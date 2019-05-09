2d648db28c265389fa8da5b439c5a606b03180be
Lingxiao-Wang <46420917+Lingxiao-Wang@users.noreply.github.com>
2019-05-09 16:09:34 -0400

    Add files via upload

--- src/simulation/simulation.R
+++ src/simulation/simulation.R
@@ -23,6 +23,7 @@ seed2 = seednumber1[,1]
 #seed1 = seeds[, 1]
 #seed2 = seeds[, 2]
 
+
 # Population generation
 N =100000
 set.seed(9762342)
@@ -35,7 +36,7 @@ v_w$w9 = (v_w$v4*0.67+v_w$v9*0.33)*1.317
 pop=v_w[,c(1:4, 11:14, 9, 10)]
 pop = pop[,order(as.numeric(substr(names(pop), 2, 3)))]
 names(pop) = paste0(rep("w", 10), c(1:10), sep = "")
-round(cor(pop), 1)
+#round(cor(pop), 1)
 beta  = c(0, 1, 1, 1.5, 1.5, -.8, -.5, .7)
 n.beta=length(beta)
 alpha = c(-2.5, 1, 1, 1, 1, .71, -.19, .26)
@@ -56,42 +57,56 @@ pop$w5_w7 = .5*pop$w5*pop$w7
 pop$w1_w6 = .5*pop$w1*pop$w6
 pop$w2_w3 = .7*pop$w2*pop$w3
 pop$w3_w4 = .5*pop$w3*pop$w4
-pop$w2_3 = pop$w2^3
-pop$w4_3 = pop$w4^3
-pop$w1_w2_w3 = .4*pop$w1*pop$w2*pop$w3
-pop$w2_2_w4_2 = 0.4*pop$w2_2*pop$w4_2
-
-pop$w1_c = as.numeric(cut(pop$w1, c(-5, -1, 0, 1, 6)))
-pop$w2_c = as.numeric(cut(pop$w2, c(-5, -1.5, -1, -0.5, 0.5, 1, 1.5, 5)))
-pop$w3_c = as.numeric(cut(pop$w2, c(-5, 0, 5)))
-pop$w4_c = as.numeric(cut(pop$w2, c(-5,-2, 2, 5)))
-pop$w5_s = sqrt(abs(pop$w5))
-pop$w6_e = exp(pop$w6/2)
-pop$w7_l = log(pop$w7+5)
+pop$w3_2_w5_2 = 0.6*pop$w3^2*pop$w5^2
+pop$w1_w2_w3  = 0.6*pop$w1*pop$w2*pop$w3
+pop$w4_w5_w7  = 0.6*pop$w4*pop$w5*pop$w7
 
 
+pop$w1_c = cut(pop$w1, c(-5, -1, 0, 1, 6))
+pop$w2_c = cut(pop$w2, c(-5, -1, 0, 1, 6))
+pop$w3_c = cut(pop$w3, c(-5, -1, 0, 1, 6))
+pop$w4_c = cut(pop$w4, c(-5, -1, 0, 1, 6))
+
+
+pop$w1_s = 2*sqrt(abs(pop$w1+pop$w2+pop$w3))
+pop$w2_s = 2*sqrt(abs(pop$w2+pop$w3+pop$w4))
+pop$w3_s = 2*sqrt(abs(pop$w3+pop$w4+pop$w5))
+pop$w4_s = 2*sqrt(abs(pop$w4+pop$w5+pop$w7))
+#round(cor(pop[,c(1:24,32:35)]),2)
+
+pop$w1_n = pop$w1+rnorm(N)/10+pop$w8/10
+pop$w2_n = pop$w2+rnorm(N)+pop$w9/10
+pop$w3_n = pop$w3+rnorm(N)+pop$w10/10
+pop$w4_n = pop$w4+rnorm(N)+pop$y/2
+#round(cor(pop[,c(1:24,36:39)]),2)
+
+
+ds.m_9 = model.matrix(as.formula(paste("y~",paste(names(pop)[28:31],collapse="+"))), data = pop)
+beta_9 = c(0, -0.8, -0.5, 0.5, 0.2, 2, 0.5, 3, 1, 0.5, -1, 2, 0.5)
 
 covars   = names(pop)[1:7]
 c_covars = names(pop)[1:7]
 f_covars = NULL
 if(sum(!(covars%in%c_covars))) f_covars=covars[!(covars%in%c_covars)]
 
-odds=matrix(0, N, 9)
-odds[,1] = exp(as.matrix(cbind(1, pop[,c(1:7)]))%*%matrix(beta, n.beta, 1))
-odds[,2] = exp(as.matrix(cbind(1, pop[,c(1:7, 12)]))%*%matrix(beta[c(1:8, 3)], n.beta+1, 1))
-odds[,3] = exp(as.matrix(cbind(1, pop[,c(1:7, 12:14)]))%*%matrix(beta[c(1:8, 3, 5, 8)], n.beta+3, 1))
-odds[,4] = exp(as.matrix(cbind(1, pop[,c(1:7, 15:18)]))%*%matrix(beta[c(1:8, 2, 3, 5, 6)], n.beta+4, 1))
-odds[,5] = exp(as.matrix(cbind(1, pop[,c(1:7, 12, 15:18)]))%*%matrix(beta[c(1:8, 3, 2, 3, 5, 6)], n.beta+5, 1))
-odds[,6] = exp(as.matrix(cbind(1, pop[,c(1:7, 15, 16, 19:24, 17, 18)]))%*%matrix(beta[c(1:8, 2:6, 2:6)], n.beta+10, 1))
-odds[,7] = exp(as.matrix(cbind(1, pop[,c(1:7, 12:14, 15, 16, 19:24, 17, 18)]))%*%matrix(beta[c(1:8, 3, 5, 8, 2:6, 2:6)], n.beta+13, 1))
-odds[,8] = exp(as.matrix(cbind(1, pop[,c(1:7, 12:14, 15, 16, 19:24, 17, 18, 25:28)]))%*%matrix(beta[c(1:8, 3, 5, 8, 2:6, 2:6, 3, 5, 2, 4)], n.beta+17, 1))
-odds[,9] = exp(as.matrix(cbind(1, pop[,c(29:35)]))%*%matrix(beta[c(1:8)], n.beta, 1))
+odds=matrix(0, N, 10)
+odds[,1]  = exp(as.matrix(cbind(1, pop[,c(1:7)]))%*%matrix(beta, n.beta, 1))
+odds[,2]  = exp(as.matrix(cbind(1, pop[,c(1:7, 12)]))%*%matrix(beta[c(1:8, 3)], n.beta+1, 1))
+odds[,3]  = exp(as.matrix(cbind(1, pop[,c(1:7, 12:14)]))%*%matrix(beta[c(1:8, 3, 5, 8)], n.beta+3, 1))
+odds[,4]  = exp(as.matrix(cbind(1, pop[,c(1:7, 15:18)]))%*%matrix(beta[c(1:8, 2, 3, 5, 6)], n.beta+4, 1))
+odds[,5]  = exp(as.matrix(cbind(1, pop[,c(1:7, 12, 15:18)]))%*%matrix(beta[c(1:8, 3, 2, 3, 5, 6)], n.beta+5, 1))
+odds[,6]  = exp(as.matrix(cbind(1, pop[,c(1:7, 15, 16, 19:24, 17, 18)]))%*%matrix(beta[c(1:8, 2:6, 2:6)], n.beta+10, 1))
+odds[,7]  = exp(as.matrix(cbind(1, pop[,c(1:7, 12:14, 15, 16, 19:24, 17, 18)]))%*%matrix(beta[c(1:8, 3, 5, 8, 2:6, 2:6)], n.beta+13, 1))
+odds[,8]  = exp(as.matrix(cbind(1, pop[,c(1:7, 12:14, 15:18, 25:27)]))%*%matrix(beta[c(1:8, 3, 5, 8, 2, 3, 5, 6, 4, 2, 3)], n.beta+10, 1))
+#odds[,9]  = exp(as.matrix(cbind(1, pop[,c(1:4, 32:35)]))%*%matrix(beta[c(1:5, 2:5)], 9, 1))
+odds[,9] = exp(ds.m_9%*%matrix(beta_9, length(beta_9), 1))
+odds[,10] = exp(as.matrix(cbind(1, pop[,c(36:39)]))%*%matrix(beta[1:5], 5, 1))
 
 #q = as.data.frame(odds/(1+odds))
-q_c = cbind(odds[,1]^0.3,   odds[,2]^0.25,  odds[,3]^0.2,   odds[,4]^0.27,  odds[,5]^0.25, 
-            odds[,6]^0.22,  odds[,7]^0.17,  odds[,8]^0.08,  odds[,9]^0.35)
-q_s = cbind(odds[,1]^-0.2,  odds[,2]^-0.2,  odds[,3]^-0.15, odds[,4]^-0.17, odds[,5]^-0.17, 
-            odds[,6]^-0.15, odds[,7]^-0.13, odds[,8]^-0.05, odds[,9]^-0.25)
+q_c = cbind(odds[,1]^0.3,   odds[,2]^0.25,  odds[,3]^0.25,  odds[,4]^0.27,  odds[,5]^0.25, 
+            odds[,6]^0.22,  odds[,7]^0.17,  odds[,8]^0.18,  odds[,9]^0.4,   odds[,10]^0.23)
+q_s = cbind(odds[,1]^-0.2,  odds[,2]^-0.2,  odds[,3]^-0.2,  odds[,4]^-0.17, odds[,5]^-0.17, 
+            odds[,6]^-0.15, odds[,7]^-0.13, odds[,8]^-0.11, odds[,9]^-0.26, odds[,10]^-0.15)
 
 Formulas = c("trt~w1+w2+w3+w4+w5+w6+w7",
              "trt~w1+w2+w3+w4+w5+w6+w7+w2_2",
@@ -100,26 +115,34 @@ Formulas = c("trt~w1+w2+w3+w4+w5+w6+w7",
              "trt~w1+w2+w3+w4+w5+w6+w7+w2_2+w1_w3+w2_w4+w4_w5+w5_w6",
              "trt~w1+w2+w3+w4+w5+w6+w7+w1_w3+w2_w4+w4_w5+w5_w6+w3_w5+w4_w6+w5_w7+w1_w6+w2_w3+w3_w4",
              "trt~w1+w2+w3+w4+w5+w6+w7+w2_2+w4_2+w7_2+w1_w3+w2_w4+w4_w5+w5_w6+w3_w5+w4_w6+w5_w7+w1_w6+w2_w3+w3_w4",
-             "trt~w1+w2+w3+w4+w5+w6+w7+w2_2+w4_2+w7_2+w1_w3+w2_w4+w4_w5+w5_w6+w3_w5+w4_w6+w5_w7+w1_w6+w2_w3+w3_w4+w2_3+w4_3+w1_w2_w3+w2_2_w4_2",
-             "trt~w1_c+w2_c+w3_c+w4_c+w5_s+w6_e+w7_l")
+             "trt~w1+w2+w3+w4+w5+w6+w7+w2_2+w4_2+w7_2+w1_w3+w2_w4+w4_w5+w5_w6+w3_w5+w4_w6+w5_w7+w1_w6+w2_w3+w3_w4+w3_2_w5_2+w1_w2_w3+w4_w5_w7",
+             #"trt~w1+w2+w3+w4+w1_s+w2_s+w3_s+w4_s",
+             "trt~w1_c+w2_c+w3_c+w4_c",
+             "trt~w1_n+w2_n+w3_n+w4_n")
 
 fm.p = update(as.formula(Formulas[1]), ~.^2)
+
+
 
 NSIMU=5
 n_c = 5000
 n_s = 5000
-est = array(0, c(NSIMU, 15, length(Formulas))#,
-            #dimnames = list(c(1:NSIMU), c("Naive", "wtd chrt", "wtd svy", "IPSW(true)", "IPSW(main)", "KW (true)", 
-            #                              "KW(main)", "KW (MOB)", "KW (RF)", "KW (XTree)", "KW (GBM)"),
-            #                paste0(rep("model", 7), c(1:7), sep=""))
+est = array(0, c(NSIMU, 15, length(Formulas)),
+            dimnames = list(c(1:NSIMU), c("cht", "cht_w", "svy_w", 
+                                          "ipsw_t", "ipsw_m", "ipsw_i", 
+                                          "kw_t", "kw_m", "kw_i", "kw_mob", 
+                                          "kw_rf", "kw_crf", "kw_xtree", 
+                                          "kw_gbm", "kw_mboost"),
+                            paste0(rep("model", 10), c(1:10), sep=""))
 )
-
 wt_m = est
 wt_v = est
+smd_all = est
+
 p_score.c=NULL
 p_score.s=NULL
-for (k in 8:9){
-
+for (k in 1:10){
+#k=1
   for(simu in 1:NSIMU){
 #simu=1
     samp.c = samp.slct(seed = seed1[simu], 
@@ -149,10 +172,14 @@ for (k in 8:9){
     # Name of data source indicator in the combined sample 
     rsp_name="trt" # 1 for AARP, 0 for NHIS
     # Covariate balance before adjustment
-    tab_pre_adjust <- bal.tab(psa_dat[, covars], treat = psa_dat$trt, weights = psa_dat$wt, 
+    tab_pre_adjust <- bal.tab(psa_dat[, covars], treat = psa_dat$trt, weights = psa_dat$elig_wt, 
                               s.d.denom = "pooled", binary = "std", method="weighting")
+
+    smd_all[simu,1, k] = mean(abs(tab_pre_adjust$Balance[, "Diff.Adj"]))
+
+    smd_all[simu,2, k] = mean(abs(bal.tab(psa_dat[, covars], treat = psa_dat$trt, weights = psa_dat$wt, 
+                                          s.d.denom = "pooled", binary = "std", method="weighting")$Balance[, "Diff.Adj"]))
     
-    psa_dat$wt_kw <- psa_dat$elig_wt
     # IPSW method
     #True propensity score model
     ds = svydesign(ids=~1, weight = ~ elig_wt, data = psa_dat)
@@ -181,11 +208,14 @@ for (k in 8:9){
     for(i in 1:n_pw){
       # calculate KW weights
       ipsw[,i] = ipsw.wt(p_score.c = p_score.w.c[,i], svy.wt = samp.s$wt)
+      psa_dat$wt_kw = psa_dat$elig_wt
+      psa_dat$wt_kw[psa_dat$trt == 1] <- ipsw[,i]
+      smd_all[simu,3+i, k] = mean(abs(bal.tab(psa_dat[, covars], treat = psa_dat$trt, weights = psa_dat$wt_kw, 
+                                              s.d.denom = "pooled", binary = "std", method = "weighting")$Balance[, "Diff.Adj"]))
       est[simu,3+i, k]  = sum(samp.c$y*ipsw[,i])/sum(ipsw[,i])
       wt_m[simu,3+i, k] = mean(ipsw[,i])
       wt_v[simu,3+i, k] = var(ipsw[,i])
     }
-    
     # Kernel weighting method
     kw = as.data.frame(matrix(0, n_c, 9))
     ###########################################################################
@@ -200,6 +230,9 @@ for (k in 8:9){
     p_score.s = data.frame(lgst = p_score[psa_dat[,rsp_name]==0])
     kw[,1] = kw.wt(p_score.c = p_score.c[,1], p_score.s = p_score.s[,1], 
                    svy.wt= samp.s$wt, Large=F)$pswt
+    psa_dat$wt_kw[psa_dat$trt == 1] <- kw[,1]
+    smd_all[simu,7, k] = mean(abs(bal.tab(psa_dat[, covars], treat = psa_dat$trt, weights = psa_dat$wt_kw, 
+                                            s.d.denom = "pooled", binary = "std", method = "weighting")$Balance[, "Diff.Adj"]))
     wt_m[simu,7, k] = mean(kw[,1])
     wt_v[simu,7, k] = var(kw[,1])                      
     est[simu, 7, k] = sum(samp.c$y*kw[,1])/sum(kw[,1])
@@ -215,6 +248,9 @@ for (k in 8:9){
     p_score.s = cbind(p_score.s, lgst_m = p_score[psa_dat[,rsp_name]==0])
     kw[,2] = kw.wt(p_score.c = p_score.c[,2], p_score.s = p_score.s[,2], 
                    svy.wt= samp.s$wt, Large=F)$pswt
+    psa_dat$wt_kw[psa_dat$trt == 1] <- kw[,2]
+    smd_all[simu,8, k] = mean(abs(bal.tab(psa_dat[, covars], treat = psa_dat$trt, weights = psa_dat$wt_kw, 
+                                          s.d.denom = "pooled", binary = "std", method = "weighting")$Balance[, "Diff.Adj"]))
     wt_m[simu,8, k] = mean(kw[,2])
     wt_v[simu,8, k] = var(kw[,2])                      
     est[simu, 8, k] = sum(samp.c$y*kw[, 2])/sum(kw[, 2])
@@ -230,6 +266,9 @@ for (k in 8:9){
     p_score.s = cbind(p_score.s, lgst_m = p_score[psa_dat[,rsp_name]==0])
     kw[,3] = kw.wt(p_score.c = p_score.c[,3], p_score.s = p_score.s[,3], 
                    svy.wt= samp.s$wt, Large=F)$pswt
+    psa_dat$wt_kw[psa_dat$trt == 1] <- kw[,3]
+    smd_all[simu,9, k] = mean(abs(bal.tab(psa_dat[, covars], treat = psa_dat$trt, weights = psa_dat$wt_kw, 
+                                          s.d.denom = "pooled", binary = "std", method = "weighting")$Balance[, "Diff.Adj"]))
     wt_m[simu,9, k] = mean(kw[,3])
     wt_v[simu,9, k] = var(kw[,3])                      
     est[simu, 9, k] = sum(samp.c$y*kw[, 3])/sum(kw[, 3])
@@ -281,6 +320,7 @@ for (k in 8:9){
     p_score.c = cbind(p_score.c, mob = p_score_c.tmp[, best])
     p_score.s = cbind(p_score.s, mob = p_score_s.tmp[, best])
     kw[, 4] = samp.c[,names(samp.c) == paste0("kw.mob.", best)]
+    smd_all[simu,10, k] = smds[1+best]
     wt_m[simu,10, k] = mean(kw[,4])
     wt_v[simu,10, k] = var(kw[,4])                      
     est[simu, 10, k] = sum(samp.c$y*kw[, 4])/sum(kw[, 4])
@@ -324,6 +364,7 @@ for (k in 8:9){
     p_score.c = cbind(p_score.c, rf = p_score_c.tmp[, best])
     p_score.s = cbind(p_score.s, rf = p_score_s.tmp[, best])	
     kw[, 5] = samp.c[,names(samp.c) == paste0("kw.rf.", best)]
+    smd_all[simu,11, k] = smds[best]
     wt_m[simu,11, k] = mean(kw[,5])
     wt_v[simu,11, k] = var(kw[,5])                      
     est[simu, 11, k] = sum(samp.c$y*kw[, 5])/sum(kw[, 5])
@@ -365,6 +406,7 @@ for (k in 8:9){
     p_score.c = cbind(p_score.c, crf = p_score_c.tmp[, best])
     p_score.s = cbind(p_score.s, crf = p_score_s.tmp[, best])	
     kw[, 6] = samp.c[,names(samp.c) == paste0("kw.crf.", best)]
+    smd_all[simu,12, k] = smds[best]
     wt_m[simu,12, k] = mean(kw[,6])
     wt_v[simu,12, k] = var(kw[,6])                      
     est[simu, 12, k] = sum(samp.c$y*kw[, 6])/sum(kw[, 6])
@@ -410,6 +452,7 @@ for (k in 8:9){
     p_score.c = cbind(p_score.c, xtree = p_score_c.tmp[, best])
     p_score.s = cbind(p_score.s, xtree = p_score_s.tmp[, best])	
     kw[, 7] = samp.c[,names(samp.c) == paste0("kw.xtree.", best)]
+    smd_all[simu,13, k] = smds[best]
     wt_m[simu,13, k] = mean(kw[,7])
     wt_v[simu,13, k] = var(kw[,7])                      
     est[simu, 13, k] = sum(samp.c$y*kw[, 7])/sum(kw[, 7])
@@ -476,6 +519,7 @@ for (k in 8:9){
     p_score.c = cbind(p_score.c, gbm = p_score_o_c[, best])
     p_score.s = cbind(p_score.s, gbm = p_score_o_s[, best])	
     kw[, 8] = samp.c[,names(samp.c) == paste0("kw.gbm.o", best)]
+    smd_all[simu,14, k] = smds_o[best]
     wt_m[simu,14, k] = mean(kw[,8])
     wt_v[simu,14, k] = var(kw[,8])                      
     est[simu, 14, k] = sum(samp.c$y*kw[, 8])/sum(kw[, 8])
@@ -521,14 +565,16 @@ for (k in 8:9){
       names(samp.c)[dim(samp.c)[2]] <- paste0("kw.mboost.", i)
       # Check improvement in covariate balance
       if (abs(smds[i] - smds[i+1]) < 0.001 | length(tune_mstop) == i){
-        print(paste0("mboost", i))
+        print(i)
         break
       }
     }
     # Select KW weights with best average covariate balance
-    p_score.c = cbind(p_score.c, mboost = p_score_c.tmp[, ifelse(i == 1, i, i-1)])
-    p_score.s = cbind(p_score.s, mboost = p_score_s.tmp[, ifelse(i == 1, i, i-1)])
-    kw[, 9] = samp.c[,names(samp.c) == paste0("kw.mboost.", ifelse(i == 1, i, i-1))]
+    best <- which.min(smds[2:(length(tune_mstop)+1)])
+    p_score.c = cbind(p_score.c, mboost = p_score_c.tmp[, best])
+    p_score.s = cbind(p_score.s, mboost = p_score_s.tmp[, best])
+    kw[, 9] = samp.c[,names(samp.c) == paste0("kw.mboost.", best)]
+    smd_all[simu,15, k] = smds[best+1]
     wt_m[simu,15, k] = mean(kw[,9])
     wt_v[simu,15, k] = var(kw[,9])                      
     est[simu, 15, k] = sum(samp.c$y*kw[, 9])/sum(kw[, 9])
@@ -551,4 +597,5 @@ seed_k = cbind(node = kk, seed= seed1)
 write.table(as.data.frame.table(wt_v), "/home/wangl29/kw_ml_r/wt_v.txt", append = T, row.names = F, col.names = F)
 write.table(as.data.frame.table(wt_m), "/home/wangl29/kw_ml_r/wt_m.txt", append = T, row.names = F, col.names = F)
 write.table(as.data.frame.table(est) , "/home/wangl29/kw_ml_r/est.txt" , append = T, row.names = F, col.names = F)
+write.table(as.data.frame.table(smd_all) , "/home/wangl29/kw_ml_r/smd_all.txt" , append = T, row.names = F, col.names = F)
 
